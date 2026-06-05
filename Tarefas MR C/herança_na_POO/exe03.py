f617b784d50399e0b2c5aadc3a19b2a67a01b7f8
gustavodiassouza19-3C <gustavo.dias.souza19@escola.pr.gov.br>
2026-06-05 14:56:54 -0300

    Update exe03.py

--- Tarefas MR C/herança_na_POO/exe03.py
+++ Tarefas MR C/herança_na_POO/exe03.py
@@ -4,7 +4,7 @@ class Funcionario:
         self.salario = salario
 
     def exibir(self):
-        print(f"Funcionário: {self.nome} | Salário Base: R$ {self.salario:.2f}")
+        print(f"Funcionário: {self.nome} | Salário Base: R$ {self.salario}")
 
 class Gerente(Funcionario):
     def __init__(self, nome, salario, bonus):
@@ -15,14 +15,14 @@ class Gerente(Funcionario):
         return self.salario + self.bonus
 
     def exibir(self):
-        print(f"Gerente: {self.nome} | Salário Total: R$ {self.salario_total():.2f}")
+        print(f"Gerente: {self.nome} | Salário Total: R$ {self.salario_total()}")
 
 
 
-func01 = Funcionario("Carlos Silva", 3000.00)
+func01 = Funcionario("Carlos", 3000.00)
 func01.exibir()
 
-gerente01 = Gerente("Ana Souza", 6000.00, 2500.00)
+gerente01 = Gerente("Ana ", 6000.00, 2500.00)
 gerente01.exibir()
 
 total = gerente01.salario_total()
